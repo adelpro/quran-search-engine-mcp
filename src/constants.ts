1a23b0dfac10edd9c7c4a416ca7e6e6a9323afd6
webbrain-one <295484252+webbrain-one@users.noreply.github.com>
2026-08-02 05:25:08 +0300

    Extract search limit constants
    Move hardcoded default and max search limits to a centralized
    constants file.

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+/**
+ * Default number of results returned per page.
+ */
+export const DEFAULT_PAGE_LIMIT = 10;
+
+/**
+ * Maximum number of results allowed per search request.
+ */
+export const MAX_SEARCH_LIMIT = 200;
